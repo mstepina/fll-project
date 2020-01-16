3df60e3812384ba29912c1aea7c5c71a2ee5f5ff
stsong <stsong@gap.com>
2020-01-15 22:02:34 -0800

    [@ssong] init db setup
    1) init db setup with one record

--- setup/setupDb.sql
+++ setup/setupDb.sql
@@ -1,4 +1,3 @@
-
 create table students
 (
     first_name      varchar,
@@ -9,11 +8,39 @@ create table students
     biography       text,
     date_of_birth   date,
     competitiveness integer default 0,
-    createdat       date,
-    modifedat       date,
+    "createdAt"     date    default now(),
+    "updatedAt"     date    default now(),
     experience      integer,
-    location        varchar
+    location        varchar,
+    group_id        integer
+        constraint students_groups_id_fk
+            references groups
+            on update cascade on delete set null,
+    email           varchar
 );
 
-INSERT INTO public.students (first_name, last_name, id, biography, date_of_birth, competitiveness, createdat, modifedat, experience, location) VALUES ('Steven', 'SONG', 1, 'Im a highschool student looking to join a team', '2020-01-16', 1, '2020-01-16', '2020-01-16', 10, '6600 Dumbarton Circle');
+alter table students
+    owner to postgres;
 
+INSERT INTO public.students (first_name, last_name, id, biography, date_of_birth, competitiveness, "createdAt", "updatedAt", experience, location, group_id, email) VALUES ('Steven', 'SONG', 1, 'Im a highschool student looking to join a team', '2020-01-16', 1, '2020-01-16', '2020-01-16', 10, '6600 Dumbarton Circle', null, null);
+
+create table groups
+(
+    id                  serial not null
+        constraint groups_pk
+            primary key,
+    name                varchar,
+    location            varchar,
+    avg_competitiveness integer default 0,
+    experience          integer default 0,
+    "createdAt"         date    default now(),
+    "updatedAt"         date    default now()
+);
+
+alter table groups
+    owner to postgres;
+
+create unique index groups_id_uindex
+    on groups (id);
+
+INSERT INTO public.groups (id, name, location, avg_competitiveness, experience, "createdAt", "updatedAt") VALUES (1, 'ROBOGROUP', '6600 Dumbarton Circle Fremont', 1, 1, '2020-01-16', '2020-01-16');
