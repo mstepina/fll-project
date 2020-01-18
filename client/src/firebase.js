fad814444a9dca508d537486271e36593cd41525
stsong <stsong@gap.com>
2020-01-18 14:57:29 -0800

    [@ssong] fixed folder structure
    1) work primarily in react accessing the db then we can serve from firebase with the hosting.

---
+++ client/src/firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase';
+
+
+const config = {
+    apiKey: "AIzaSyBMXRNqyOpJMZkXyxJRx2m2FuOg5gMWm74",
+    authDomain: "fll-project.firebaseapp.com",
+    databaseURL: "https://fll-project.firebaseio.com",
+    projectId: "fll-project",
+    storageBucket: "fll-project.appspot.com",
+    messagingSenderId: "921010624293",
+    appId: "1:921010624293:web:32455da33e835b1f952ccc",
+    measurementId: "G-SSXGSDJ4FK"
+};
+firebase.initializeApp(config);
+firebase.analytics();
+const databaseRef = firebase.database().ref();
+
+export const todosRef = databaseRef.child("todos")
+// Your web app's Firebase configuration
